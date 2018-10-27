6456c9de2c96fbfde5a0c2229c008c76274b6f29
emifervi <emifervi@hotmail.com>
2018-10-27 12:04:21 -0500

    Made the changes for the main functionality and added servo

--- ProyectoFinal.ino
+++ ProyectoFinal.ino
@@ -1,3 +1,8 @@
+//anadir la libreria
+#include <servo.h>
+
+//Sirven para el servo
+Servo motorcito;
 //Sirve para el LED RGB
 int patitaRoja = 12;
 int patitaVerde = 4;
@@ -14,7 +19,6 @@ int in2 = 7;
 int in3 = 11;
 int in4 = 3;
 
-
 void ultrasonico(){
   digitalWrite(trigger,LOW);
   delayMicroseconds(5);
@@ -26,24 +30,28 @@ void ultrasonico(){
   Serial.println(distancia);
   delay(10);
 }
+
 void puenteZitoAvanza(){
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
 }
+
 void puenteZitoReverZa(){
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
 }
+
 void puenteZitoZtop(){
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
 }
+
 //Girar el Carro a la derecha
 void puenteZitoDerecha(){
   digitalWrite(in3, HIGH);
@@ -51,44 +59,71 @@ void puenteZitoDerecha(){
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
 }
+
 void puenteZitoZurdo(){
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
 }
+
 void giro(){
   double d1, d2;
-  delay(300);
+  // gira a la izquierda el Ultrasonico
+  motorcito.write(-90);
   ultrasonico();
   d1 = distancia;
-  delay(300);
+  // gira a la derecha el Ultrasonico
+  motorcito.write(180);
   ultrasonico();
   d2 = distancia;
-  //falta hacer las comparaciones de la mayor...
+
+  //decide la distancia
+  motorcito.write(-90);
+  if(d1 > d2){
+    puenteZitoDerecha();
+  }
+  else{
+    puenteZitoZurdo();
+  }
 }
+
 void estatus(int color){
   if (color  == 1){
     digitalWrite(patitaRoja, HIGH);
     digitalWrite(patitaVerde, LOW);
     digitalWrite(patitaAzul, LOW);
-  }else{
+  }
+  else{
     digitalWrite(patitaRoja, LOW);
     digitalWrite(patitaVerde, HIGH);
     digitalWrite(patitaAzul, LOW);
   }
 }
-void avanza(){
+
+void arcoiriZ(){
   //Hacer arcoiris con los leds;
+  digitalWrite(patitaRoja, HIGH);
+  delay(100);
+  digitalWrite(patitaRoja, LOW);
+  digitalWrite(patitaVerde, HIGH);
+  delay(100);
+  digitalWrite(patitaVerde, LOW);
+  digitalWrite(patitaAzul, HIGH);
+  delay(100);
+  digitalWrite(patitaAzul, LOW);
   
 }
+
 void setup() {
+  // declarar el servo
+  motorcito.attach(6);
   // put your setup code here, to run once:
   pinMode(led, OUTPUT);
   Serial.begin(9600);
   pinMode(trigger, OUTPUT);
   pinMode(echo, INPUT);
-  //puenteZito
+  // puenteZito
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
   pinMode(in1, OUTPUT);
@@ -98,12 +133,11 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   ultrasonico();
-  if(distancia < 20){
-    digitalWrite(led, HIGH);
+  if(distancia < 7){
     puenteZitoZtop();
-    puenteZitoReverZa();
-  }else{
-    digitalWrite(led, LOW);
+    giro();
+  }
+  else{
     puenteZitoAvanza();
   }  
 }
